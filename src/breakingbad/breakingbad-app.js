3cde82bb262ee9ba9004c87a6506087760676088
Lesnier Garcia Estrada <lesier.estrada@gmail.com>
2024-04-30 00:18:30 -0400

    Add fetch to BreakingBadQouteAPI

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -1,11 +1,26 @@
+const URL = 'https://api.breakingbadquotes.xyz/v1/quotes';
 
+/**
+ * @returns {Promise<any>}
+ */
+const fetchQuote = async () => {
+
+    const res = await fetch(URL);
+    const data = await res.json();
+
+    console.log(data[0]);
+    return data[0];
+}
 
 /**
  * 
  * @param {HTMLElement} element 
  */
-export const BreakingBadApp = (element) => {
+export const BreakingBadApp = async (element) => {
 
     document.querySelector('#app-title').innerText = 'BreakingBad App';
     element.innerText = 'Loading.....';
+
+    const quoteFetched = await fetchQuote();
+    element.innerHTML = `<em>${quoteFetched.quote}<em>`;
 }
